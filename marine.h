94329bde589ebf4dfda5ba47ec1bb6e375e50eb7
loukleon2021 <loukleon2021@gmail.com>
2016-11-20 17:25:53 -0600

    updating my stuff
    Doing work

--- marine.h
+++ marine.h
@@ -12,38 +12,26 @@
 #include "infantry.h"
 
 
-class battleship: virtual public vessel
+class marine: virtual public infantry
 {
     public:
-        battleship(string vname="Unknown", string vteam="Unknown", string vtype="Battle Ship", 
-                       int main=200, int cruise=35, int battery=5000);
+        marine();
+        marine (string tempName = "unknown", string tempType = "marine");
         
-        void set_main_gun(int);
-        int get_main_gun() const;
+        void setAssaultRifle(int);
+        int  getAssaultRifle() const;
+        virtual void speak() const;
+        virtual void display() const;
+        virtual void attack(infantry*);
+        virtual void renderAid(infantry*);
+        virtual void getAttacked(int);
+        virtual void receiveAid(int);
+        virtual void die();
         
-        void set_cruise_missile(int);
-        int get_cruise_missile() const;
-        
-        void set_battery_gun(int);
-        int get_battery_gun() const;
-        
-        void fire_main_gun(vessel*);
-        void launch_cruise_missile(vessel*);
-        void fire_battery_gun(vessel*);
-        
-        virtual void light_attack(vessel*);
-        virtual void heavy_attack(vessel*);
-        virtual void sink();
-
-        void print() const;
-        void display() const;
-                
+                 
     private:
-        int main_gun;
-        int cruise_missile;
-        int battery_gun;
-        
-};
-
+        int assaultRifle;
+    
+};    
 
 #endif
